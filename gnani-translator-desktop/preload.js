234fbf95de47227a335eb71516570d94b7e8b9d6
Shine-5705 <guptashine5002@gmail.com>
2026-04-08 14:03:28 +0530

    now working just teh latency needs to be improved

--- gnani-translator-desktop/preload.js
+++ gnani-translator-desktop/preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   startTranslation: (config) => ipcRenderer.send('start-translation', config),
   stopTranslation: () => ipcRenderer.send('stop-translation'),
   sendAudioChunk: (chunkBytes) => ipcRenderer.send('audio-chunk', chunkBytes),
+  sendMicActivity: (payload) => ipcRenderer.send('mic-activity', payload),
   onTranslationStatus: (callback) => {
     ipcRenderer.on('translation-status', (_event, payload) => callback(payload));
   },
