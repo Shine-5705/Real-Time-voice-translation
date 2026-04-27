7679c71ff03c95a0694f30343ca3e9481b884c7b
Shine-5705 <guptashine5002@gmail.com>
2026-04-27 18:14:06 +0530

    organised and all langugae translation using deepgram an d vachana

--- gnani-translator-desktop-pro-whisper-stt/src/main/index.js
+++
@@ -1,6 +0,0 @@
-require('./config/env');
-const { app } = require('electron');
-const { createMainWindow } = require('./windows/createMainWindow');
-const { bootRuntime } = require('./pipeline/runtime');
-
-bootRuntime({ app, createMainWindow });
